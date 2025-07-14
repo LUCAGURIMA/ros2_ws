9cb28dbed96a0ebe08c5bbeb4a1856ff1719689e
LUCAGURIMA <lucanewage@gmail.com>
2025-07-14 14:17:37 -0300

    commit

--- src/apriltag_ros/src/pose_estimation.hpp
+++ src/apriltag_ros/src/pose_estimation.hpp
@@ -4,22 +4,8 @@
 #include <functional>
 #include <geometry_msgs/msg/transform_stamped.hpp>
 #include <unordered_map>
-#include <array>
-#include <opencv2/core.hpp>
-#include <geometry_msgs/msg/pose_stamped.hpp>
-#include <rclcpp/rclcpp.hpp>
-#include <std_msgs/msg/header.hpp>
+
 
 typedef std::function<geometry_msgs::msg::Transform(apriltag_detection_t* const, const std::array<double, 4>&, const double&)> pose_estimation_f;
 
 extern const std::unordered_map<std::string, pose_estimation_f> pose_estimation_methods;
-
-// Função utilitária para logar pose
-void log_pose(const cv::Mat& tvec, const cv::Mat& rvec);
-
-// Função para publicar pose
-void publish_pose(
-    const cv::Mat& tvec,
-    const cv::Mat& rvec,
-    const rclcpp::Publisher<geometry_msgs::msg::PoseStamped>::SharedPtr& pose_pub,
-    const std_msgs::msg::Header& header);
